18c9c64aada370520619006e47d9e981ae57c35f
BenKungu <benwaweru2013@gmail.com>
2023-03-21 14:22:37 -0700

    printing char

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* *
+ *include standard input/output library
+ *
+ *print the string "_putchar" followed by a new line
+ *
+ *return: Always 0  (success)
+*/
+
+int main() {
+    printf("_putchar\n");
+    return 0;
+}
+
