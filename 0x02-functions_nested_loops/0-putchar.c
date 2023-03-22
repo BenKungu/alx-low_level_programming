6b2e9777693782a92bd763fdd722a22f6d63baa2
BenKungu <benwaweru2013@gmail.com>
2023-03-22 16:33:22 -0700

    style

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,16 +6,16 @@
  *Desciption : print _putchar followed by a new line
  *
  *Return: 0  (Success)
-*/
+ */
 int main()
 {
-   char s[] ="_putchar";
-   int i = 0;
-   while(s[i])
-   {
-	_putchar(s[i]);
-	i++;
-   }
-   _putchar('\n');
-   return 0;
+       	char s[] ="_putchar";
+       	int i = 0;
+       	while(s[i])
+       	{
+		_putchar(s[i]);
+		i++;
+       	}
+       	_putchar('\n');
+	return 0;
 }
